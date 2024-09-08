d7400d612f5f319142125d139b777a69a2ce6c73
Laurie Anderson <laurieanderson92@hotmail.co.uk>
2024-09-08 09:53:28 +0000

    build: added excepts to post tiles

--- codestar/settings.py
+++ codestar/settings.py
@@ -18,18 +18,17 @@ if os.path.isfile('env.py'):
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
-TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
 ALLOWED_HOSTS = ['8000-laurieanders-djangoblog-6bnkt05b5io.ws.codeinstitute-ide.net', '.herokuapp.com',]
 
 
